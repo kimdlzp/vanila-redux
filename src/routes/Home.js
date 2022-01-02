34d2a43fee58fd0af4e278b280188b3be062bea7
kimdlzp <kimdlzp@gmail.com>
2022-01-02 16:58:48 +0900

    Deleting To Do

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
 function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
-  function onsubmit(e) {
+  function onSubmit(e) {
     e.preventDefault();
     addToDo(text);
     setText("");
@@ -15,11 +16,15 @@ function Home({ toDos, addToDo }) {
   return (
     <>
       <h1>To Do</h1>
-      <form onsubmit={onsubmit}>
+      <form onSubmit={onSubmit}>
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
